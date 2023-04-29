7f5da7058bf83064cfad2b1244097dfce24643ce
Puneet Kumar <117968792+Puneet6938@users.noreply.github.com>
2023-04-29 09:07:04 +0530

    Add files via upload

---
+++ swappin.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter A and B: ";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"value of A and B is: "<<a<<"  "<<b<<endl;
+    return 0;
+} 
